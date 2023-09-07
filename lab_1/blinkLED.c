44a9628a3dbc42c86bae292eafe93e947f7d7672
POLaforcade <po.laforcade@gmail.com>
2023-09-07 16:12:08 -0400

    test

--- lab_1/blinkLED.c
+++ lab_1/blinkLED.c
@@ -19,28 +19,28 @@ int main(int argc, char* argv[]){
       printf("Please choose [1] : GPIO Pin, [2] : number of time LED should blink\n");
       return 2;
    }
-   printf("%s", argv[1]);
-   int nb_gpio = atoi(argv[1]);
-   int nb_blink = atoi(argv[2]);
-   printf("test");
-   char* str_gpio;
-   sprintf(str_gpio, "%d/", nb_gpio);
-   printf("Starting the blink LED program %d times on GPIO %d\n", nb_blink, nb_gpio);
-   // Setup the path to the correct GPIO
-   strcat(GPIO_FILE, str_gpio);
-   // setup GPIO pin
-   printf("Setting up the LED on the GPIO\n");
-   writeGPIO(GPIO_SYSFS "export", str_gpio);
-   usleep(100000);
-   writeGPIO(GPIO_FILE "direction", "out");
-   // blink
-   for(int i=0; i<nb_blink; i++)
-   {
-      writeGPIO(GPIO_FILE "value", "1");
-      usleep(500000);
-      writeGPIO(GPIO_FILE "value", "0");
-      usleep(500000);
-   }
-   // unexport GPIO pin
-   writeGPIO(GPIO_SYSFS "unexport", str_gpio);
+   // printf("%s", argv[1]);
+   // int nb_gpio = atoi(argv[1]);
+   // int nb_blink = atoi(argv[2]);
+   // printf("test");
+   // char* str_gpio;
+   // sprintf(str_gpio, "%d/", nb_gpio);
+   // printf("Starting the blink LED program %d times on GPIO %d\n", nb_blink, nb_gpio);
+   // // Setup the path to the correct GPIO
+   // strcat(GPIO_FILE, str_gpio);
+   // // setup GPIO pin
+   // printf("Setting up the LED on the GPIO\n");
+   // writeGPIO(GPIO_SYSFS "export", str_gpio);
+   // usleep(100000);
+   // writeGPIO(GPIO_FILE "direction", "out");
+   // // blink
+   // for(int i=0; i<nb_blink; i++)
+   // {
+   //    writeGPIO(GPIO_FILE "value", "1");
+   //    usleep(500000);
+   //    writeGPIO(GPIO_FILE "value", "0");
+   //    usleep(500000);
+   // }
+   // // unexport GPIO pin
+   // writeGPIO(GPIO_SYSFS "unexport", str_gpio);
 }
