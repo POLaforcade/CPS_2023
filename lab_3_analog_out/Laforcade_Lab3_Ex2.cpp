da1392413428829d238eae83f25ebff4abf074c9
POLaforcade <po.laforcade@gmail.com>
2023-09-21 17:43:06 -0400

    Added Lab3 Ex 2

---
+++ lab_3_analog_out/Laforcade_Lab3_Ex2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <wiringPi.h>
+
+using namespace std;
+
+#define PWM0 12
+#define PIN_BUTTON 18
+
+int cnt = 0;
+int N;
+
+void myISR(void)
+{
+   delay(10);
+
+   if (digitalRead(PIN_BUTTON) == 0){
+      cnt += 1;
+      if(cnt>N)
+         cnt = 0;
+      pwmWrite(PWM0, (128/N)*cnt);
+   }
+
+   delay(50);
+}
+
+int main(int argc, char **argv){
+
+   if(argc!=2){
+       printf("Please choose [1] : N, the steps to full brightness\n");
+       return 2;
+   }
+
+   N = atoi(argv[1]);
+   
+   wiringPiSetupPhys();
+   pinMode(PWM0, PWM_OUTPUT);
+   pinMode(PIN_BUTTON, INPUT);
+   pullUpDnControl(PIN_BUTTON, PUD_DOWN);
+
+   cout << "Button pin has been setup." << endl;
+
+   pwmSetMode(PWM_MODE_MS);
+   pwmSetRange(128);
+   pwmSetClock(15);
+
+   cout << "The PWM Output is enabled" << endl;
+
+   if(wiringPiISR(PIN_BUTTON, INT_EDGE_FALLING, myISR) < 0){
+      printf("ISR setup error!\n");
+      return -1;
+   }
+
+   while(1){
+      // Loop 
+   }
+}
