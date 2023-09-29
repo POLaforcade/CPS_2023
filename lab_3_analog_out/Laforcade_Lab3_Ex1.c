ca13307f3622144fa92d564b471f4b6118930073
POLaforcade <po.laforcade@gmail.com>
2023-09-29 12:44:11 -0400

    m1

--- lab_3_analog_out/Laforcade_Lab3_Ex1.c
+++ lab_3_analog_out/Laforcade_Lab3_Ex1.c
@@ -26,8 +26,8 @@ int main(int argc, char **argv)
     int LedPin[3] = {RED_PIN, GREEN_PIN, BLUE_PIN};
     int R, G, B;
     extractRGBFromString(argv[1], &R, &G, &B);
-    R = R-0xFF0000;
-    G = G-0xFF00,
+    R = R;
+    G = G,
     B = B;
 
     wiringPiSetupGpio();
