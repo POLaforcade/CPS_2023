d3319d38ebc00ffd5d44899a0c6a3fc7682a7e86
POLaforcade <po.laforcade@gmail.com>
2023-11-25 14:59:29 -0500

    Servo 2.0

--- Project/servomotor.hpp
+++ Project/servomotor.hpp
@@ -4,23 +4,36 @@
 #include <iostream>
 #include <wiringPi.h>
 
-#define PWM0 18
+#define SERVO_PIN 18
 
-class servomotor
-{
+class servomotor {
 
-public: 
-    servomotor()
-    {
-        pinMode(PWM0, OUTPUT);
+private:
+
+    static const int SERVO_MIN_PWM = 100;
+    static const int SERVO_MAX_PWM = 200;
+
+    int map(int value, int fromLow, int fromHigh, int toLow, int toHigh) const {
+        return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
+    }
+public:
+    ServoMotor() {
+        pinMode(SERVO_PIN, PWM_OUTPUT);
+
         pwmSetMode(PWM_MODE_MS);
-        pwmSetRange(180);
-        pwmSetClock(2133);
+        pwmSetClock(192);
+        pwmSetRange(2000);
     }
 
-    void set(int angle)
-    {
-        pwmWrite(PWM0, angle);
+    void setAngle(int angle) {
+        if (angle < 0) {
+            angle = 0;
+        } else if (angle > 180) {
+            angle = 180;
+        }
+
+        int pwmValue = map(angle, 0, 180, SERVO_MIN_PWM, SERVO_MAX_PWM);
+        pwmWrite(SERVO_PIN, pwmValue);
     }
 };
 
