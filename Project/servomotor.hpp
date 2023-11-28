7a54162547385ca0aa93ca80d670dcfa18d2f3bc
POLaforcade <po.laforcade@gmail.com>
2023-11-28 13:24:15 -0500

    removed langage object

--- Project/servomotor.hpp
+++ Project/servomotor.hpp
@@ -6,35 +6,29 @@
 
 #define SERVO_PIN 18
 
-class servomotor {
+const int SERVO_MIN_PWM = 100;
+const int SERVO_MAX_PWM = 300;
 
-private:
+int map(int alpha, int alpha_min, int alpha_max, int pulse_min, int pulse_max) const {
+    return (((pulse_max-pulse_min)*(alpha-alpha_min))/(alpha_max-alpha_min))+pulse_min;
+}
 
-    static const int SERVO_MIN_PWM = 100;
-    static const int SERVO_MAX_PWM = 300;
+void servomotor_setup() {
+    pinMode(SERVO_PIN, PWM_OUTPUT);
+    pwmSetMode(PWM_MODE_MS);
+    pwmSetClock(192);
+    pwmSetRange(2000);
+}
 
-    int map(int alpha, int alpha_min, int alpha_max, int pulse_min, int pulse_max) const {
-        return (((pulse_max-pulse_min)*(alpha-alpha_min))/(alpha_max-alpha_min))+pulse_min;
+void setAngle(int angle) {
+    if (angle < 0) {
+        angle = 0;
+    } else if (angle > 180) {
+        angle = 180;
     }
-public:
-    servomotor() {
-        pinMode(SERVO_PIN, PWM_OUTPUT);
 
-        pwmSetMode(PWM_MODE_MS);
-        pwmSetClock(192);
-        pwmSetRange(2000);
-    }
-
-    void setAngle(int angle) {
-        if (angle < 0) {
-            angle = 0;
-        } else if (angle > 180) {
-            angle = 180;
-        }
-
-        int pwmValue = map(angle, 0, 180, SERVO_MIN_PWM, SERVO_MAX_PWM);
-        pwmWrite(SERVO_PIN, pwmValue);
-    }
-};
+    int pwmValue = map(angle, 0, 180, SERVO_MIN_PWM, SERVO_MAX_PWM);
+    pwmWrite(SERVO_PIN, pwmValue);
+}
 
 #endif
