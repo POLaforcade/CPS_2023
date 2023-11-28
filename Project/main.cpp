6bffc673e2302efad471a6d26b677d99cca03e9f
POLaforcade <po.laforcade@gmail.com>
2023-11-28 14:02:59 -0500

    test

--- Project/main.cpp
+++ Project/main.cpp
@@ -78,6 +78,7 @@ int main(void)
                 c = getKey();
                 if(c == 'A'){
                     int tmp = read_code();
+                    printf("le code a été demande\n");
                     if(tmp == code){
                         std::cout << "Valid Code\nUnlocked chest, Press[A] for lock " << std::endl;
                         mode = UNLOCKED;
