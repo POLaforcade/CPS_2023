d42a9d7a0389e04427a00e30d09d36792a8e509b
POLaforcade <po.laforcade@gmail.com>
2023-11-30 11:23:24 -0500

    test

--- Project/test_servomotor.cpp
+++ Project/test_servomotor.cpp
@@ -2,6 +2,12 @@
 
 int main()
 {
+    // Setting up GPIO with wiringPi
+    if(wiringPiSetupGpio() == -1){ //when initialize wiring failed,print message to screen
+        std::cout << "setup wiringPi failed !" << std::endl;
+        return -1;
+    }
+
     servomotor_setup();
     setAngle(180);
     return 0;
